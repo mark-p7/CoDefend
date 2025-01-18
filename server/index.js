828f60eb8213dce6a2ffd1e3ec22e89688f23b17
mark-p7 <markdeguzman1142@gmail.com>
2025-01-18 15:07:33 -0800

    Created main func and express api routes

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const FileHandler = require('./getFile');
+const Scanner = require('./scanner/index');
+
+app.get('/', async (req, res) => {
+    res.send("Server is running");
+});
+
+app.post("/scan", async (req, res) => {
+
+    const { url } = req.body;
+
+    if (!url) {
+        res.status(400).send("No URL provided");
+        return;
+    }
+
+    const fileHandler = new FileHandler();
+
+    const file = fileHandler.downloadFile(url);
+
+    const scanner = new Scanner(file, { virusTotal: true });
+    scanner.scanFileforViruses();
+
+    res.send(result);
+    fileHandler.clearFile()
+});
+
+const main = async () => {
+    console.log("Starting server...");
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+    // Create File
+    const fileHandler = new FileHandler();
+    const res = await fileHandler.downloadFile("https://images.pexels.com/photos/20263436/pexels-photo-20263436/free-photo-of-revel-atlantic-city-hotel-in-atlantic-city-in-usa.jpeg");
+
+    const scanner = new Scanner(res.file, { virusTotal: true });
+    scanner.scanFileforViruses();
+
+    // fileHandler.clearFile();
+
+
+}
+
+main();
+
+
+
+
+
